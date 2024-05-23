e5626d942d24fae1e698b92e37c19f2789ccf718
Cheesecakeyyy <104256610+Cheesecakeyyy@users.noreply.github.com>
2024-05-23 22:07:31 +0800

    5th trial

---
+++ Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame studentsFrame = new JFrame();
+        studentsFrame.add(new StudentsPanel());
+        studentsFrame.pack();
+        studentsFrame.setVisible(true);
+
+
+        //course frame
+        JFrame courseFrame = new JFrame();
+        courseFrame.add(new CoursePanel());
+        courseFrame.pack();
+        courseFrame.setVisible(true);
+
+    }
+}
---
+++ StudentsTableModel.java
@@ -0,0 +1,50 @@
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+
+public class StudentsTableModel extends AbstractTableModel {
+
+    public ArrayList<Student> list;
+
+    public StudentsTableModel() {
+        list = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        list.add(student);
+    }
+
+
+    public void removeStudent( int index ) {
+        list.remove(index);
+    }
+
+    @Override
+    public int getRowCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 2;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        String[] columns = new String[]{"ID","NAME"};
+        return columns[column];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Student student = list.get(rowIndex);
+        if ( columnIndex == 0 ) {
+            return student.getId();
+        } else if ( columnIndex == 0 ) {
+            return student.getName();
+        } else {
+            return null;
+        }
+
+        }
+    }
+
